004852dc12e7a9e0f1df708d488e1e6b9b17fb74
theteladras <sanel.hadzini@yahoo.com>
2018-10-10 01:17:04 +0200

    added test for GET request

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -4,9 +4,26 @@ const request = require('supertest');
 const { app } = require('../index');
 const Todo = require('../models/todo');
 
+const todos = [
+  {
+    text: 'blabla bla'
+  },
+  {
+    text: 'haha ha'
+  },
+  {
+    text: 'yo yoyo'
+  }
+];  //dummy docsuments
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
-});
+  //Todo.remove({}).then(() => done()); //remove all documents from db and finish up the setup
+  Todo.remove({}).
+    then(() => {
+      return Todo.insertMany(todos);
+    }).
+    then( () => done() );
+}); // remove all documents from db add dummy documents and finish setup
 
 describe('POST /todos', () => {
   it('creating a new todo', (done) => {
@@ -23,7 +40,7 @@ describe('POST /todos', () => {
         if(e) {
           return done(e);
         }
-        Todo.find().
+        Todo.find({ text }).
           then( docs => {
             expect(docs.length).toBe(1);
             expect(docs[0].text).toBe(text);
@@ -44,11 +61,21 @@ describe('POST /todos', () => {
         }
         Todo.find().
           then( docs => {
-            expect(docs.length).toBe(0);
+            expect(docs.length).toBe(3);
             done();
           }).
           catch( e => done(e) );
       });
   });
 
+});
+
+describe('GET /todos', () => {
+  it('get list items', (done) => {
+    request(app).
+      get('/todos').
+      expect(200).
+      expect( res => expect(res.body.todos.length).toBe(3) ).
+      end(done);
+  });
 });
