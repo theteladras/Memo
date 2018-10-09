ddc58ebf44c18884f35c53a43d764ab6e6c5ef74
theteladras <sanel.hadzini@yahoo.com>
2018-10-10 00:36:47 +0200

    added first GET route, requiering db list data

--- server/index.js
+++ server/index.js
@@ -14,8 +14,15 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   });
   todo.save().then((document) => {
-    res.send(document);
+    res.send(document);  //returning the saved body to the client
   }, e => res.status(400).send(e));
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then( docs => {
+    res.send({ todos: docs });  //returning all db results to the client
+  }).
+  catch( e => res.status(400).send(e));
 });
 
 app.listen(3000, () => console.log('Server runing on port 3000.'));
