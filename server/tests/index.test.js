2227bdf17f47afc1856081d966041e7217cef60e
theteladras <sanel.hadzini@yahoo.com>
2018-10-22 00:05:30 +0200

    test for new GET route --> /todos/:id

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -1,17 +1,21 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../index');
 const Todo = require('../models/todo');
 
 const todos = [
   {
+    _id: new ObjectID(),
     text: 'blabla bla'
   },
   {
+    _id: new ObjectID(),
     text: 'haha ha'
   },
   {
+    _id: new ObjectID(),
     text: 'yo yoyo'
   }
 ];  //dummy docsuments
@@ -76,6 +80,34 @@ describe('GET /todos', () => {
       get('/todos').
       expect(200).
       expect( res => expect(res.body.todos.length).toBe(3) ).
+      end(done);
+  });
+});
+
+describe('Testing GET .../todos/:id', () => {
+  it('should return documents', (done) => {
+    request(app).
+      get(`/todos/${todos[0]._id.toHexString()}`).
+      expect(200).
+      expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      }).
+      end(done);
+  });
+
+  it('should return 404 for non existing todo', (done) => {
+    let hexID = new ObjectID().toHexString();
+    request(app).
+      get(`/todos/${hexID}`).
+      expect(404).
+      end(done);
+  });
+
+  it('should return 404 for non object id', (done) => {
+    let hexID = new ObjectID().toHexString();
+    request(app).
+      get('/todos/qwerty').
+      expect(404).
       end(done);
   });
 });
