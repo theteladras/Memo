6717c1557d66bafc4b39b63bcf11af118aa6c938
theteladras <sanel.hadzini@yahoo.com>
2018-10-22 01:22:27 +0200

    setup for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ToDo', { useNewUrlParser: true });
+mongoose.connect( process.env.dbURL || 'mongodb://localhost:27017/ToDo', { useNewUrlParser: true });
 
 module.exports = { mongoose };
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const Todo = require('./models/todo');
 const User = require('./models/user');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -37,6 +38,6 @@ app.get('/todos/:clientId', (req, res) => {
     catch( e => res.status(400).send() );
 });
 
-app.listen(3000, () => console.log('Server runing on port 3000.'));
+app.listen(port, () => console.log(`Server runing on port ${port}.`));
 
 module.exports = { app };
