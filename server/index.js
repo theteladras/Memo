be9bc1abdb25bd088f603db47e8b7a23ff81f29b
theteladras <sanel.hadzini@yahoo.com>
2018-10-23 02:07:40 +0200

    added PATCH route for updating single object

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const Todo = require('./models/todo');
@@ -47,6 +48,25 @@ app.delete('/todos/:clientId', (req, res) => {
       res.send({ todo: doc });
     }).
     catch( e => res.status(400).send() );
+});
+
+app.patch('/todos/:clientId', (req, res) => {
+  let id = req.params.clientId;
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+  let body = _.pick(req.body, ['text', 'completed']);
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  }
+  else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).
+    then( doc => {
+      if (!doc) return res.status(404).send();
+      res.send({todo: doc});
+    }).
+    catch( e => {console.log(e); res.status(400).send()} )
 });
 
 app.listen(port, () => console.log(`Server runing on port ${port}.`));
