844d9921c7c8ee0c636d53964e4a60f712262f3b
theteladras <sanel.hadzini@yahoo.com>
2018-10-28 02:48:26 +0200

    added GET /users/me, added authentication middleware

--- server/index.js
+++ server/index.js
@@ -7,12 +7,31 @@ require('./db/config');
 const { mongoose } = require('./db/mongoose');
 const Todo = require('./models/todo');
 const User = require('./models/user');
+const auth = require('./middleware/auth');
 
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  let user = new User(body);
+
+  user.save().
+    then(() => {
+      return user.generateAuthToken();
+    }).
+    then(token => {
+      res.header('x-auth', token).send(user);
+    }).
+    catch(e => res.status(400).send(e));
+});
+
+app.get('/users/me', auth, (req, res) => {
+  res.send(req.user);
+});
 
 app.post('/todos', (req, res) => {
   let todo = new Todo({
--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,67 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-let User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email address'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    }
+  }]
 });
+
+UserSchema.methods.generateAuthToken = function() {
+  let user = this;
+  let access = 'auth';
+  let token = jwt.sign({ _id: user._id.toHexString(), access }, 'abcabc').toString();
+  user.tokens = user.tokens.concat([{ access, token }]);
+  return user.save().
+    then(() => {
+      return token;
+    });
+};
+
+UserSchema.statics.findByToken = function(token) {
+  let User = this;
+  let decoded;
+  try {
+    decoded = jwt.verify(token, 'abcabc');
+  }
+  catch (e) {
+    return Promise.reject();
+  }
+  return User.findOne({ _id: decoded._id, 'tokens.token': token, 'tokens.access': 'auth' });
+};
+
+UserSchema.methods.toJSON = function() {
+  let user = this;
+  let userObj = user.toObject();
+  return _.pick(userObj, ['_id', 'email']);
+};
+
+let User = mongoose.model('User', UserSchema);
 
 module.exports = User;
 
