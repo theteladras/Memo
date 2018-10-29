25e83bb1f0711fa4a8147508ec80de0ef5136f44
theteladras <sanel.hadzini@yahoo.com>
2018-10-29 12:42:18 +0100

    reloacted the first middleware, db-s adjusting

---
+++ server/db/config.js
@@ -0,0 +1,10 @@
+// creating two separate databases, for dev and test
+let env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+  process.env.PORT = 3000;
+  process.env.dbURL = 'mongodb://localhost:27017/ToDo';
+}
+else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.dbURL = 'mongodb://localhost:27017/ToDoTest';
+}
